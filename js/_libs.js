e6e064ac2c91cb16e8f299c68d823ca9312f1359
Cicadas3301 <59025608+Cicadas3301@users.noreply.github.com>
2023-03-22 23:59:15 +0200

    Add files via upload

---
+++ js/_libs.js
@@ -0,0 +1,12 @@
+// Lazy loading img & background images using intersection observer
+// Reference: https://developers.google.com/web/fundamentals/performance/lazy-loading-guidance/images-and-video/
+// Using example: <img class="lazy" src="thumb.gif" data-src="real-img.jpg" data-srcset="real-img@1x.jpg 1x, real-img@2x.jpg 2x">
+// Background image class usign example: <div class="lazy-background"> with added class ".visible" for styling
+// Background image style attribute lazy loading example: <div data-bg="image.jpg">
+
+// delete window.IntersectionObserver; // Fallback Testing
+
+document.addEventListener('DOMContentLoaded', function() {
+
+});
+
